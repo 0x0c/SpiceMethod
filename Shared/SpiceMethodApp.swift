20687791f29d5178014fa1ba89506da85910ed3a
Akira <akira.matsuda@me.com>
2020-12-05 15:52:29 +0900

    Initial Commit

---
+++ Shared/SpiceMethodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiceMethodApp.swift
+//  Shared
+//
+//  Created by Akira Matsuda on 2020/12/05.
+//
+
+import SwiftUI
+
+@main
+struct SpiceMethodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
